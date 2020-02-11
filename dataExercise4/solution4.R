b0b1fd383a0fc097b0008fb198561f7e2638b89f
otidin <onurtidin@gmail.com>
2020-02-11 23:34:58 +0100

    initial commit

---
+++ dataExercise4/solution4.R
@@ -0,0 +1,38 @@
+setwd("/Users/lindadib/Work/SIB-BCF/work/SIB_tutoring/Dib_EA/2019/dataExercice4")
+
+
+
+require(clusterProfiler)
+library(org.Hs.eg.db)
+keytypes(org.Hs.eg.db)
+
+table       <-read.csv("HS_pvalues.csv")
+SCORE       <-table$score
+names(SCORE)<-table$gene.ENTREZ.ID
+SCORE       <-sort(SCORE ,decreasing=T)
+
+ego <- gseGO(geneList     = SCORE,
+              OrgDb        = org.Hs.eg.db,
+              ont          = "ALL",
+              nPerm        = 1000,
+	    pvalueCutoff = 1,
+              verbose      = FALSE)
+
+head(ego)             
+library("DOSE")         
+gseaplot(ego, geneSetID="GO:0048518")
+dotplot(ego, showCategory=30)
+
+#########
+egoBP <- gseGO(geneList     = SCORE,
+               OrgDb        = org.Hs.eg.db,
+               ont          = "BP",
+               nPerm        = 1000,
+	       pvalueCutoff = 1,
+               verbose      = FALSE)
+
+head(egoBP)                   
+gseaplot(egoBP, geneSetID="GO:0048518")
+dotplot(egoBP, showCategory=30)
+plotGOgraph(egoBP)
+emapplot(egoBP)
